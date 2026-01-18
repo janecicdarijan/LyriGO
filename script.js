9d77911d1477f20247d0e6d1f00f4ad648b6747d
Darijan <janecicdarijan@gmail.com>
2026-01-18 14:49:46 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//index.html//
+  const dugme = document.getElementById('nastavi');
+
+  if (dugme) {
+      dugme.addEventListener('click', () => {
+          
+          dugme.classList.toggle('btn-aktivan');
+      });
+  }
+//index.html end//
